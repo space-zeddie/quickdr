3cd2b9ba04dd7506b10c0bf018ce0da8ef944625
mzakharuk <mzakharuk@cloudlinux.com>
2017-04-10 13:12:25 +0300

    added mvcconfig

---
+++ src/main/java/com/zakharuk/quickdr/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.zakharuk.quickdr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by matvii on 10.04.17.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
