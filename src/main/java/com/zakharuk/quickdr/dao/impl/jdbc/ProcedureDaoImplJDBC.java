f19a9f0af2bf37cf3928cc6b49ee0c8d292587bf
mzakharuk <mzakharuk@cloudlinux.com>
2017-04-12 23:33:54 +0300

    added row mappers for procedure

--- src/main/java/com/zakharuk/quickdr/dao/impl/jdbc/ProcedureDaoImplJDBC.java
+++ src/main/java/com/zakharuk/quickdr/dao/impl/jdbc/ProcedureDaoImplJDBC.java
@@ -1,10 +1,16 @@
 package com.zakharuk.quickdr.dao.impl.jdbc;
 
 import com.zakharuk.quickdr.dao.ProcedureDao;
+import com.zakharuk.quickdr.entity.Patient;
 import com.zakharuk.quickdr.entity.Procedure;
+import com.zakharuk.quickdr.pojo.ProcedurePojo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by matvii on 12.04.17.
@@ -44,4 +50,22 @@ public class ProcedureDaoImplJDBC implements ProcedureDao {
         System.out.println("Removing: " + procedure);
         jdbcTemplate.update(DELETE, procedure.getId());
     }
+
+    private RowMapper<ProcedurePojo> procedurePojoRowMapper = new RowMapper<ProcedurePojo>() {
+        public ProcedurePojo mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new ProcedurePojo(rs.getString("type"),
+                    rs.getDate("date"),
+                    rs.getBoolean("completed"));
+        }
+    };
+
+    private RowMapper<Procedure> mapper = new RowMapper<Procedure>() {
+        public Procedure mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Procedure procedure = new Procedure();
+            procedure.setType(rs.getString("type"));
+            procedure.setDateOfProcedure(rs.getDate("date"));
+            procedure.setCompleted(rs.getBoolean("completed"));
+            return procedure;
+        }
+    };
 }
