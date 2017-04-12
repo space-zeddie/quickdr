51ab60179cea8cca3f614931bcd08cfdd0619fdf
mzakharuk <mzakharuk@cloudlinux.com>
2017-04-12 22:39:03 +0300

    added patientController

--- src/main/java/com/zakharuk/quickdr/controller/PatientController.java
+++ src/main/java/com/zakharuk/quickdr/controller/PatientController.java
@@ -1,7 +1,374 @@
 package com.zakharuk.quickdr.controller;
 
+import com.zakharuk.quickdr.entity.ChildPatient;
+import com.zakharuk.quickdr.entity.Patient;
+import com.zakharuk.quickdr.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by matvii on 12.04.17.
  */
+@Controller
 public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    /**
+     * GET /create  --> Create a new patient and save it in the database.
+     */
+    @RequestMapping("/register-patient")
+    @ResponseBody
+    public String create(String name, int age, String diagnosis) {
+        String patientId = "";
+        try {
+            Patient patient = new ChildPatient(name, age);
+            patient.setDiagnosis(diagnosis);
+            patientService.addPatient(patient);
+            patientId = String.valueOf(patient.getPatientId());
+        }
+        catch (Exception ex) {
+            return HEADER +  "Error registering the partient: " + ex.toString() + FOOTER;
+        }
+        return HEADER + "Patient succesfully registered with id = " + patientId + FOOTER;
+    }
+
+    /**
+     * GET /delete  --> Delete the patient having the passed id.
+     */
+    @RequestMapping("/delete")
+    @ResponseBody
+    public String delete(int id) {
+        try {
+            Patient patient = new ChildPatient();
+            patient.setPatientId(id);
+            patientService.remove(patient);
+        }
+        catch (Exception ex) {
+            return HEADER + "Error deleting the patient:" + ex.toString() + FOOTER;
+        }
+        return HEADER + "Patient succesfully deleted!" + FOOTER;
+    }
+
+    /**
+     * GET /get-by-name  --> Return the id for the patient having the passed
+     * name.
+     */
+    /*@RequestMapping("/get-by-name")
+    @ResponseBody
+    public String getByName(String name) {
+        String subjectId = "";
+        try {
+            Subject subject = subjectDao.findByName(name);
+            subjectId = String.valueOf(subject.getId());
+        }
+        catch (Exception ex) {
+            return HEADER + "Subject not found" + FOOTER;
+        }
+        return HEADER + "The subject id is: " + subjectId + FOOTER;
+    }*/
+
+    /**
+     * GET /update  --> Update the name and the age for the subject in the
+     * database having the passed id.
+     */
+    @RequestMapping("/update")
+    @ResponseBody
+    public String updateSubject(int id, String name, int age, String diagnosis) {
+        try {
+            Patient patient = patientService.getPatientById(id);
+            patient.setName(name);
+            patient.setAge(age);
+            patient.setDiagnosis(diagnosis);
+            patientService.savePatient(patient);
+        }
+        catch (Exception ex) {
+            return HEADER + "Error updating the patient: " + ex.toString() + FOOTER;
+        }
+        return HEADER + "Patient succesfully updated!" + FOOTER;
+    }
+
+    /**
+     * GET /update  --> Update the name and the credits for the subject in the
+     * database having the passed id.
+     */
+   /* @RequestMapping("/set-prof")
+    @ResponseBody
+    public String setProf(long id, String prof) {
+        try {
+            Subject subject = subjectDao.findOne(id);
+            subject.setProf(prof);
+            subjectDao.save(subject);
+        }
+        catch (Exception ex) {
+            return HEADER + "Error updating the subject: " + ex.toString() + FOOTER;
+        }
+        return HEADER + "Subject succesfully updated!" + FOOTER;
+    }*/
+
+    /*@RequestMapping("/recommend")
+    @ResponseBody
+    public String recommend(long id, boolean rec) {
+        try {
+            Subject subject = subjectDao.findOne(id);
+            subject.setRecommended(rec);
+            subjectDao.save(subject);
+        }
+        catch (Exception ex) {
+            return HEADER + "Error updating the subject: " + ex.toString() + FOOTER;
+        }
+        return HEADER + "Subject succesfully updated!" + FOOTER;
+    }*/
+
+
+
+   /* @RequestMapping("/all-patients")
+    @ResponseBody
+    public String findAll() {
+        StringBuilder res = new StringBuilder();
+        res.append(HEADER);
+        try {
+            Iterable<Subject> all = subjectDao.findAll();
+            res = listSubjects(all, res);
+        }
+        catch (Exception ex) {
+            res.append("Error retrieving the subjects: " + ex.toString());
+        }
+        finally {
+            res.append(FOOTER);
+            return res.toString();
+        }
+    }*/
+
+    /*@RequestMapping("/all-recommended")
+    @ResponseBody
+    public String findAllRecommended() {
+        StringBuilder res = new StringBuilder();
+        res.append(HEADER);
+        try {
+            List<Subject> all = new ArrayList<Subject>();
+            for (Subject s : subjectDao.findAll())
+                if (s.isRecommended()) all.add(s);
+            res = listSubjects(all, res);
+        }
+        catch (Exception ex) {
+            res.append("Error retrieving the subjects: " + ex.toString());
+        }
+        finally {
+            res.append(FOOTER);
+            return res.toString();
+        }
+    }*/
+
+    /*private StringBuilder listSubjects(Iterable<Subject> is, StringBuilder res) {
+        for (Subject s : is) {
+            res.append("<div>");
+            res.append(s.toString());
+            res.append("<br>");
+            res.append(listStudentsBtn(s.getId()));
+            if (SecurityConfiguration.isStudent() && s.isRecommended() && ZakharukApplication.isSecondStage())
+                res.append(signUpBtn(s.getId()));
+            if (SecurityConfiguration.isMethodist() && ZakharukApplication.isFirstStage()) {
+                if (!s.isRecommended())
+                    res.append(recommendBtn(s.getId()));
+                else
+                    res.append(unrecommendBtn(s.getId()));
+            }
+            if (SecurityConfiguration.isMethodist() && ZakharukApplication.isFirstStage() || SecurityConfiguration.isAdmin())
+                res.append(updateProfBtn(s.getId()));
+            if (SecurityConfiguration.isAdmin())
+                res.append(editSubjectBtn(s.getId()));
+            if (SecurityConfiguration.isAdmin())
+                res.append(deleteSubjectBtn(s.getId()));
+            res.append("</div>");
+            res.append("<br>");
+        }
+        return res;
+    }*/
+
+    @RequestMapping("/edit-patient")
+    @ResponseBody
+    public String editPatient(int id) {
+        StringBuilder res = new StringBuilder();
+        res.append(HEADER);
+        Patient patient = patientService.getPatientById(id);
+        res.append("<div class=\"jumbotron\">");
+        res.append("<div>");
+        res.append("Name <input type=\"text\" id=\"name\" name=\"name\" th:text=\"${name}\" value=\""
+                + patient.getName() +"\" /><br />");
+        res.append("Age <input type=\"text\" id=\"credits\" name=\"credits\" th:text=\"${credits}\" value=\""
+                + patient.getAge() +"\" /><br />");
+        res.append("Diagnosis <input type=\"text\" id=\"annotation\" name=\"annotation\" th:text=\"${annotation}\" value=\""
+                + patient.getDiagnosis() +"\" /><br />");
+        res.append("<a id=\"" + patient.getPatientId() + "\" href=\"/\" class = \"btn btn-info lnk_edit\">Update</a>");
+        res.append(FOOTER);
+        return res.toString();
+    }
+
+   /* @RequestMapping("/list-students")
+    @ResponseBody
+    public String listStudents(long id) {
+        StringBuilder res = new StringBuilder();
+        res.append(HEADER);
+        try {
+            Subject s = subjectDao.findOne(id);
+            for (User u : s.getStudents()) {
+                res.append(u.toString());
+                res.append("<br>");
+                if (SecurityConfiguration.isStudent() &&
+                        (u.getId() == userDao.findByName(SecurityConfiguration.findAuth().getName()).getId()))
+                    res.append(removeStudentBtn(s.getId(), u.getId()));
+                res.append("<br>");
+            }
+        }
+        catch (Exception ex) {
+            res.append("Error retrieving the students: " + ex.toString());
+        }
+        finally {
+            res.append(FOOTER);
+            return res.toString();
+        }
+    }*/
+
+
+    /*@RequestMapping("/add-user")
+    @ResponseBody
+    @Transactional
+    public String addUser(Long subjectid, Long userid) {
+        try {
+            Subject subject = subjectDao.findOne(subjectid);
+            User user = userDao.findOne(userid);
+            if (!subject.getStudents().contains(user)) {
+                subject.getStudents().add(user);
+                user.getSubjects().add(subject);
+                subjectRepository.save(new HashSet<Subject>(){{
+                    add(subject);
+                }});
+                userRepository.save(new HashSet<User>(){{
+                    add(user);
+                }});
+            }
+        }
+        catch (Exception ex) {
+            return HEADER + "Error adding the user: " + ex.toString() + FOOTER;
+        }
+        return HEADER + "User succesfully added!" + FOOTER;
+    }
+
+    @RequestMapping("/remove-user")
+    @ResponseBody
+    public String removeUser(Long subjectid, Long userid) {
+        try {
+            Subject subject = subjectDao.findOne(subjectid);
+            User user = userDao.findOne(userid);
+            if (subject.getStudents().contains(user)) {
+                user.getSubjects().remove(subject);
+                subject.getStudents().remove(user);
+                subjectRepository.save(new HashSet<Subject>(){{
+                    add(subject);
+                }});
+                userRepository.save(new HashSet<User>(){{
+                    add(user);
+                }});
+            }
+        }
+        catch (Exception ex) {
+            return "Error removing the user: " + ex.toString();
+        }
+        return "User succesfully removed!";
+    }*/
+
+    /*@RequestMapping("/select-add-student")
+    @ResponseBody
+    public String selectStudent(Long subjectid) {
+        StringBuilder res = new StringBuilder();
+        res.append(HEADER);
+        Subject subject = subjectDao.findOne(subjectid);
+        //Iterable<User> users = userDao.findAll();
+        //for (User u : users)
+        User u = userDao.findByName(SecurityConfiguration.findAuth().getName());
+        res.append("<p>You currently have " + u.getAllCredits() + " credits. Are you sure you want to sign up?</p>");
+        res.append(addStudentBtn(subjectid, u.getId()));
+        res.append(FOOTER);
+        return res.toString();
+    }*/
+
+    private String signUpBtn(long id) {
+        return "<a href=\"/select-add-student?subjectid=" + id + "\" class=\"btn btn-info\">Sign Up</a>";
+    }
+
+    private String listStudentsBtn(long id) {
+
+        return "<a href=\"/list-students?id=" + id + "\" class=\"btn btn-info\">View Students</a>";
+    }
+    private String deleteSubjectBtn(long id) {
+
+        return "<a href=\"/delete?id=" + id + "\" class=\"btn btn-info\">Delete Subject</a>";
+    }
+    private String editSubjectBtn(long id) {
+
+        return "<a href=\"/edit-subject?id=" + id + "\" class=\"btn btn-info\">Edit Subject</a>";
+    }
+
+
+    private String recommendBtn(long id) {
+
+        return "<a href=\"/recommend?id=" + id + "&rec=true" + "\" class=\"btn btn-info\">Recommend</a>";
+    }
+
+    private String unrecommendBtn(long id) {
+
+        return "<a href=\"/recommend?id=" + id + "&rec=false" + "\" class=\"btn btn-info\">Unrecommend</a>";
+    }
+
+    private String updateProfBtn(long id) {
+
+        return "<a href=\"/?id=" + id  + "\" class=\"btn btn-info updateProf\" id=\""+ id +"\">Change Professor</a>";
+    }
+
+
+
+
+    private String addStudentBtn(long id, long studentId) {
+
+        return "<a href=\"/add-user?subjectid=" + id + "&userid=" + studentId + "\" class=\"btn btn-info\">Sign Up </a>";
+    }
+    private String setProfBtn(long id, long prof) {
+
+        return "<a href=\"/set-profr?id=" + id + "&prof=" + prof + "\" class=\"btn btn-info\">Change Professor</a>";
+    }
+    private String removeStudentBtn(long id, long studentId) {
+
+        return "<a href=\"/remove-user?subjectid=" + id + "&userid=" + studentId + "\" class=\"btn btn-info\">Sign Off </a>";
+    }
+
+    protected static String HEADER = "<!DOCTYPE html>\n" +
+            "<html xmlns:th=\"http://www.thymeleaf.org\">\n" +
+            "<head lang=\"en\">\n" +
+            "\n" +
+            "    <title>Subjects</title>\n" +
+            "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>\n" +
+            "    <link href=\"css/bootstrap.min.css\"\n" +
+            "          th:href=\"@{css/bootstrap.min.css}\"\n" +
+            "          rel=\"stylesheet\" media=\"screen\" />\n" +
+            "    <script src=\"css/js/jq.js\"\n" +
+            "            th:src=\"@{css/js/jq.js}\"></script>\n" +
+            "    <script src=\"css/js/bootstrap.js\"\n" +
+            "            th:src=\"@{css/js/bootstrap.js}\"></script>\n" +
+            "    <script src=\"css/js/main.js\"\n" +
+            "            th:src=\"@{css/js/main.js}\"></script>\n" +
+            "    <link href=\"../static/css/main.css\"\n" +
+            "          th:href=\"@{css/main.css}\" rel=\"stylesheet\" media=\"screen\"/>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "<div class=\"container\">";
+
+    protected static String FOOTER = "<br><a href=\"/index\" class=\"btn btn-info\"><< Back to Homepage</a>" + "\n" +
+            "</div>\n" +
+            "</body>\n" +
+            "</html>";
+
 }
--- src/main/java/com/zakharuk/quickdr/service/impl/PatientServiceImpl.java
+++ src/main/java/com/zakharuk/quickdr/service/impl/PatientServiceImpl.java
@@ -4,10 +4,14 @@ import com.zakharuk.quickdr.dao.PatientDao;
 import com.zakharuk.quickdr.entity.Patient;
 import com.zakharuk.quickdr.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by matvii on 12.04.17.
  */
+@Service
+@Transactional
 public class PatientServiceImpl implements PatientService {
 
     @Autowired
@@ -20,7 +24,7 @@ public class PatientServiceImpl implements PatientService {
 
     @Override
     public Patient getPatientById(int id) {
-        return patientDao.getPatientById();
+        return patientDao.getPatientById(id);
     }
 
     @Override
